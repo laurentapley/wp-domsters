be82e005397095d02a3921077bacbe2dfcdd2003
Lauren Tapley <lntapley@gmail.com>
2016-11-30 16:14:38 -0800

    add browser-sync, gulpfile.js

--- page.php
+++ page.php
@@ -1,7 +1,27 @@
 <?php get_header(); ?>
-[Page.php]
-  <div id="content">
-    <h1>Welcome</h1>
-    <p id="intro">Welcome to the official website of Jay Skript and the Domsters. Here, you can <a href="about.html">learn more about the band</a>, view <a href="photos.html">photos of the band</a>, find out about <a href="live.html">tour dates</a> and <a href="contact.html">get in touch with the band</a>.</p>
-  </div>
+[page.php]
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <!-- tbs class for headers -->
+          <div class="page-header">
+            <!-- the_title() is WP function that shows the title of the post -->
+            <h1><?php the_title(); ?></h1>
+          </div>
+          <!-- WP function that outputs post content -->
+          <?php the_content(); ?>
+
+        <?php endwhile; else: ?>
+           <!-- we run this else if there is no content -->
+          <div class="page-header">
+            <h1>Wups!</h1>
+          </div>
+
+          <p>Looks like we have no content for this page?</p>
+
+        <?php endif; ?>
+      </div>
+      <?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
